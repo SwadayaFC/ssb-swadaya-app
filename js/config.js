ac895406533641f04e8f289ba9e4225ac22481b4
SwadayaFC <kotabaturt314@gmail.com>
2026-05-01 21:46:40 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // GANTI dengan URL Apps Script kamu
-const API = "https://script.google.com/macros/s/AKfycbxLFC0hN1rymW4fOfccv5JlzpFKQw9RQKjaPkAVN7oJMQLUiahSVo7K-UIqvTkTKveu/exec";
+const API = "https://script.google.com/macros/s/AKfycbz6MKxOcSdfzSCa0BDas-zzEqvLxTh_WCTxCgpIjmLo-hqIcAxcXoUKniqSKTvKjJGxWA/exec";
